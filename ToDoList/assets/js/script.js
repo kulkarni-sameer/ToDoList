5838ca0d53618f5997a213bce57889b8006004aa
kulkarni-sameer <31580460+kulkarni-sameer@users.noreply.github.com>
2018-09-10 20:39:45 -0700

    can add todos now via textbox

---
+++ ToDoList/assets/js/script.js
@@ -0,0 +1,16 @@
+$("input").on("keypress", function(event){
+	if(event.which === 13){
+		$("ul").append("<li><span>X</span> " +$("input").val() +"</li>");
+		$("input").val("");
+	}
+});
+
+$("ul").on("click", "li",function(){
+	$(this).toggleClass("done");
+});
+
+$("ul").on("click", "span", function(){
+	$(this).parent().fadeOut(function(){
+		$(this).remove();
+	})
+});
